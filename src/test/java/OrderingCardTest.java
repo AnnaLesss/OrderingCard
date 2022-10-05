3c1fed17460fa75883d62da00e322848fdd8dbe6
AnnaLesss <klatter@mail.ru>
2022-10-05 10:55:14 +0300

    ok

--- src/test/java/OrderingCardTest.java
+++ src/test/java/OrderingCardTest.java
@@ -47,4 +47,22 @@ public class OrderingCardTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
         Thread.sleep(5000);
     }
+
+    @Test
+    void InvalidName() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+        WebElement form = driver.findElement(By.cssSelector("[method=post]"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivanov Oleg");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[class=button__text]")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+        Thread.sleep(5000);
+    }
 }
